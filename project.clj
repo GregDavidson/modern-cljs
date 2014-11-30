f50274ceb8578adf5ea4b066b7f57e1ebf46c873
GregDavidson <greg.davidson+git@gmail.com>
2014-11-29 16:07:23 -0800

    finshed tutorial 3

--- project.clj
+++ project.clj
@@ -1,29 +1,33 @@
 (defproject modern-cljs "0.1.0-SNAPSHOT"
   :description "A series of tutorials on ClojureScript"
   :url "https://github.com/magomimmo/modern-cljs"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {
+             :name "Eclipse Public License"
+             :url "http://www.eclipse.org/legal/epl-v10.html" }
 
   :pom-addition [:developers [:developer
-                              [:id "magomimmo"]
-                              [:name "Mimmo Cosenza"]
-                              [:url "https://github.com/magomimmo"]
-                              [:email "mimmo.cosenza@gmail.com"]
-                              [:timezone "+2"]]]
+                               [:id "magomimmo"]
+                               [:name "Mimmo Cosenza"]
+                               [:url "https://github.com/magomimmo"]
+                               [:email "mimmo.cosenza@gmail.com"]
+                               [:timezone "+2"] ]]
 
   :min-lein-version "2.1.2"
 
-  ;; clojure source code path
   :source-paths ["src/clj" "src/cljs"]
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2069"]]
+  :dependencies [
+                  [org.clojure/clojure "1.5.1"]
+                  [org.clojure/clojurescript "0.0-2069"]
+                  [compojure "1.1.6"] ]
 
-  :plugins [[lein-cljsbuild "1.0.0"]]
+  :plugins [
+             [lein-cljsbuild "1.0.0"]
+             [lein-ring "0.8.8"] ]
 
-  ;; cljsbuild tasks configuration
-  :cljsbuild {:builds
-              [{;; clojurescript source code path
+  :ring {:handler modern-cljs.core/handler}
+  
+  :cljsbuild {:builds [{
                 :source-paths ["src/cljs"]
 
                 ;; Google Closure Compiler options
@@ -33,4 +37,4 @@
                            ;; minimum optimization
                            :optimizations :whitespace
                            ;; prettyfying emitted JS
-                           :pretty-print true}}]})
+                           :pretty-print true } }]} )
