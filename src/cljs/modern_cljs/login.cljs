c90d5657faf9450577233e35a0f8e33808514741
GregDavidson <greg.davidson+git@gmail.com>
2014-11-30 16:38:15 -0800

    simple made easy.

--- src/cljs/modern_cljs/login.cljs
+++ src/cljs/modern_cljs/login.cljs
@@ -14,7 +14,7 @@
 
 ;; define the function to attach validate-form to onsubmit event of
 ;;the form
-(defn init []
+(defn ^export init []
   ;; verify that js/document exists and that it has a getElementById
   ;; property
   (if (and js/document
@@ -24,5 +24,3 @@
     (let [login-form (.getElementById js/document "loginForm")]
       (set! (.-onsubmit login-form) validate-form))))
 
-;; initialize the HTML page in unobtrusive way
-(set! (.-onload js/window) init)
