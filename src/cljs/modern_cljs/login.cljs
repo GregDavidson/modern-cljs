d9c6acef0b4cd8fd70eb2de1e89c09b7b158e8f7
GregDavidson <greg.davidson+git@gmail.com>
2014-12-02 17:06:43 -0800

    sometimes does a submit with bad data

--- src/cljs/modern_cljs/login.cljs
+++ src/cljs/modern_cljs/login.cljs
@@ -1,22 +1,52 @@
 (ns modern-cljs.login
-  (:require [domina :refer [by-id value]]
-            [domina.events :refer [listen! prevent-default]]))
+  (:require-macros [hiccups.core :refer [html]])
+  (:require
+    [clojure.set :refer [subset?]]
+    [domina :refer [by-id by-class value append! prepend! destroy! log sel add-class!]]
+    [hiccups.runtime :as hiccupsrt]
+    [domina.events :refer [listen! prevent-default]] ) )
 
-(defn validate-form [e]
-  (let [ email (value (by-id "email"))
-         password (value (by-id "password")) ]
+(def node-ids ["email" "password" "loginForm" "submit"])
+
+(declare id-node) ;; defined in (init) below
+
+(defn id-value [id] (value (id-node id)))
+
+(def id-re {
+  "password" #"^(?=.*\d).{4,8}$" ;;; 4 to 8, at least one numeric digit
+  "email"  #"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$" })
+
+(assert (subset? (set (keys id-re)) (set node-ids)))
+  
+(defn validate-field [id]
+  (destroy! (by-class id))
+  (if (not (re-matches (id-re id) (id-value id)))
+    (do
+      (prepend! (id-node "loginForm") (html [:div.help (str "Bad " id)]))
+      (add-class! (sel "#loginForm>div.help") id)
+      false )
+    true ) )
+
+(defn validate-form [evt]
+  (let [ email (id-value "email")
+        password (id-value "password") ]
     (if (or (empty? email) (empty? password))
       (do
-        (prevent-default e)
-        (js/alert "Please, complete the form!") )
-      true ) ) )
+        (prevent-default evt)
+        (destroy! (by-class "help"))
+        (append! (id-node "loginForm") (html [:div.help "Please complete the form"])) )
+      (if (and
+            (validate-field "email")
+            (validate-field "password") )
+        true
+        (prevent-default evt) ) ) ) )
 
-;; define the function to attach validate-form to onsubmit event of
-;;the form
 (defn ^:export init []
-  ;; verify that js/document exists and that it has a getElementById
-  ;; property
-  (if (and js/document (aget js/document "getElementById"))
-    ;; get loginForm by element id and set its onsubmit property to
-    ;; our validate-form function
-    (listen! (by-id "submit") :click validate-form) ) )
+  (if (not (and js/document (aget js/document "getElementById")))
+    (js/alert "Expected document.getElementById")
+    (do
+      (assert (every? #(by-id %) node-ids)) ; check node-ids against document
+      (def id-node (zipmap node-ids (map #(by-id %) node-ids)))
+      (listen! (id-node "submit") :click validate-form)
+      (listen! (id-node "email") :blur (fn [evt] (validate-field "email")))
+      (listen! (id-node "password") :blur (fn [evt] (validate-field "password"))) ) ) )
