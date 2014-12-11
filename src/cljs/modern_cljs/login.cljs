96203aea278f308c3997357861cd397dc2c48deb
GregDavidson <greg.davidson+git@gmail.com>
2014-12-10 17:00:00 -0800

    updated some dependency versions, still have java.lang.NullPointerException :(

--- src/cljs/modern_cljs/login.cljs
+++ src/cljs/modern_cljs/login.cljs
@@ -1,9 +1,9 @@
 (ns modern-cljs.login
-<<<<<<< HEAD
   (:require-macros [hiccups.core :refer [html]])
   (:require
     [clojure.set :refer [subset?]]
-    [domina :refer [by-id by-class value append! prepend! destroy! attr log sel add-class!]]
+    [domina :refer
+      [by-id by-class value append! prepend! destroy! attr log sel add-class!] ]
     [domina.events :refer [listen! prevent-default]]
     [hiccups.runtime :as hiccupsrt] ) )
 
@@ -42,59 +42,3 @@
       (def id-node (zipmap node-ids (map #(by-id %) node-ids)))
       (listen! (id-node "submit") :click validate-form)
       (map #(listen! (id-node %) :blur (fn [evt] (validate-field %))) field-ids) ) ) )
-=======
-  (:require-macros [hiccups.core :refer [html]]
-                   [shoreleave.remotes.macros :as shore-macros])
-  (:require [domina :refer [by-id by-class value append! prepend! destroy! attr log]]
-            [domina.events :refer [listen! prevent-default]]
-            [hiccups.runtime :as hiccupsrt]
-            [modern-cljs.login.validators :refer [user-credential-errors]]
-            [shoreleave.remotes.http-rpc :refer [remote-callback]]))
-
-
-(defn validate-email-domain [email]
-  (remote-callback :email-domain-errors
-                   [email]
-                   #(if %
-                      (do
-                        (prepend! (by-id "loginForm")
-                                  (html [:div.help.email "The email domain doesn't exist."]))
-                        false)
-                      true)))
-
-(defn validate-email [email]
-  (destroy! (by-class "email"))
-  (if-let [errors (:email (user-credential-errors (value email) nil))]
-    (do
-      (prepend! (by-id "loginForm") (html [:div.help.email (first errors)]))
-      false)
-    (validate-email-domain (value email))))
-
-
-(defn validate-password [password]
-  (destroy! (by-class "password"))
-  (if-let [errors (:password (user-credential-errors nil (value password)))]
-    (do
-      (append! (by-id "loginForm") (html [:div.help.password (first errors)]))
-      false)
-    true))
-
-(defn validate-form [evt email password]
-  (if-let [{e-errs :email p-errs :password} (user-credential-errors (value email) (value password))]
-    (if (or e-errs p-errs)
-      (do
-        (destroy! (by-class "help"))
-        (prevent-default evt)
-        (append! (by-id "loginForm") (html [:div.help "Please complete the form."])))
-      (prevent-default evt))
-    true))
-
-(defn ^:export init []
-  (if (and js/document
-           (aget js/document "getElementById"))
-    (let [email (by-id "email")
-          password (by-id "password")]
-      (listen! (by-id "submit") :click (fn [evt] (validate-form evt email password)))
-      (listen! email :blur (fn [evt] (validate-email email)))
-      (listen! password :blur (fn [evt] (validate-password password))))))
->>>>>>> origin
